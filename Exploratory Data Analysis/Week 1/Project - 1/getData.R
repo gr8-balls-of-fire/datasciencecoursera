b0a427ebe4bf2fe3c06bea7b6c9e4f2270283912
gr8-balls-of-fire <parimal.mohile@gmail.com>
2016-09-05 16:25:55 +0530

    half cooked getData code 2nd edition

--- Exploratory Data Analysis/Week 1/Project - 1/getData.R
+++ Exploratory Data Analysis/Week 1/Project - 1/getData.R
@@ -1,1 +1,17 @@
+## load data.table package
+library(data.table)
+## Set Working directory
 setwd("/home/gr8-balls-of-fire/datasciencecoursera/Exploratory Data Analysis/Week 1/Project - 1")
+## set the URL string
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+## download the file using CURL method
+download.file(fileURL,destfile = "household_power_consumption.zip", method = "curl")
+## unzip the downloaded file
+unzip("household_power_consumption.zip")
+## while the filename extension is 'txt' is is a csv file with ";" as separatot
+## load the file into a data table using read.csv function 
+hpc <- data.table(read.csv("household_power_consumption.txt",sep = ";", na.strings = c("?","","NA"), header = TRUE))
+## reduce the data set to 1st & 2nd Feb 2007
+hpc <- hpc[hpc$Date=="1/2/2007"|hpc$Date=="2/2/2007",]
+
+##hpc$Date <- as.Date(hpc$Date, format = "%d/%m/%Y")
